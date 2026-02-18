6890d29927220bfa92fe6cf865a726f67c88f02f
Arman86532424 <blincikubica@gmail.com>
2026-02-18 20:22:30 +0600

    Delete Practice1/python-basics directory

--- Practice1/python-basics/list_print_example.py
+++
@@ -1,8 +0,0 @@
-x,y,z = "orange", "banana", "cherry"
-
-print(x,y,z)
-print(x + y + z)
-
-print("next one will be on the same line", end=' ')
-
-print("hello");print("world")
--- Practice1/python-basics/randomm.py
+++
@@ -1,5 +0,0 @@
-import random
-
-print(random.randrange(1,10)) #10 do not include
-print(random.randrange(1,1000000000000000000000000))
-print(random.randrange(1,2))
